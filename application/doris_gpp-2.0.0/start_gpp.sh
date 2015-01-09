dc76f7d111f30ff78353d83f76c36ceb9d34a0d0
Joel Castillo <joel.castillo@nyu.edu>
2015-01-09 18:19:00 -0500

    Updated start_gpp.sh

--- application/doris_gpp-2.0.0/start_gpp.sh
+++ application/doris_gpp-2.0.0/start_gpp.sh
@@ -1,2 +1,2 @@
 source /var/www/virtualenvs/gpp_venv/bin/activate
-uwsgi --socket /var/www/gpp/mysite.sock --chmod-socket=666 --processes=10 --wsgi-file=/var/www/gpp/django_gpp/wsgi.py --daemonize=/var/www/logs/gpp_uwsgi.log --vacuum
+uwsgi --socket /var/www/gpp/mysite.sock --chmod-socket=666 --processes=10 --wsgi-file=/var/www/gpp/django_gpp/wsgi.py --daemonize=/var/www/logs/gpp_uwsgi.log --virtualenv=/var/www/virtualenvs/gpp_venv --vacuum
