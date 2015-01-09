6a25cccd0f7512517160e18d9cfd2111b9a6ff10
Joel Castillo <joel.castillo@nyu.edu>
2015-01-09 17:42:46 -0500

    Added script to start django app

---
+++ application/doris_gpp-2.0.0/start_gpp.sh
@@ -0,0 +1,2 @@
+source /var/www/virtualenvs/gpp_venv/bin/activate
+uwsgi --socket /var/www/gpp/mysite.sock --chmod-socket=666 --processes=10 --wsgi-file=/var/www/gpp/django_gpp/wsgi.py --daemonize=/var/www/logs/gpp_uwsgi.log --vacuum
