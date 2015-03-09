b962a27e850ca0f6ab100ab16f3899f012179911
joelbcastillo <joel.castillo@nyu.edu>
2015-03-09 12:37:22 -0400

    Updating migration script; Directory paths did not save

--- scripts/db/db_migration_03092015.sh
+++ scripts/db/db_migration_03092015.sh
@@ -8,8 +8,8 @@
 export CWD=$PWD
 
 # Setup Passwords
-source ../db/prod.password_store.sh
+source $CWD/prod.password_store.sh
 
 # Re-Index the Database
-source /var/lib/mysql/virtualenvs/gpp_env/bin/activate
+source /db/mysql_data/virtualenvs/gpp_env/bin/activate
 python $CWD/../../application/index_db.py
