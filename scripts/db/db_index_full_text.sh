60492a9e8184d45932081a5fbca5c4c934106aab
estevez90 <hestevez@records.nyc.gov>
2015-04-15 11:42:10 -0400

    Script for indexing full text pdfs

---
+++ scripts/db/db_index_full_text.sh
@@ -0,0 +1,14 @@
+# Description:
+# This script performs the following operations:
+#   - Runs the elasticsearch full text index
+# PLEASE NOTE: The script should be run from the folder in which it is contained.
+
+# Source Passwords
+source prod.password_store.sh
+
+# Store Current Directory
+export CWD=$PWD
+
+# Run Index
+source /var/lib/mysql/virtualenvs/gpp_env/bin/activate
+python ../../application/index_full_text_db.py
