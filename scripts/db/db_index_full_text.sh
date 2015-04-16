f8a00e76713574a373e7b280e11e984d50ce4eca
estevez90 <hestevez@nyc.records.gov>
2015-04-16 13:31:52 -0400

    Activate and Setup Virtual Environment

--- scripts/db/db_index_full_text.sh
+++ scripts/db/db_index_full_text.sh
@@ -9,6 +9,10 @@ source ../db/prod.password_store.sh
 # Store Current Directory
 export CWD=$PWD
 
-# Run Index
+# Activate and Setup Virtual Environment
+virtualenv /var/lib/mysql/virtualenvs/gpp_env
 source /var/lib/mysql/virtualenvs/gpp_env/bin/activate
+pip install mysql-python elasticsearch
+
+# Run Index
 python ../../application/index_full_text_db.py
